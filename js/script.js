df436c77889964a7bcd8401927db9cc0fa9b7dc7
jonathanmtsz <jonathanmtsz@outlook.com>
2023-05-08 12:01:59 -0300

    aula 08/05

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
-// console.log("Olá Mundor")
-// console.log(`Resultado: ${parseInt(n1) + parseInt(n2)}`)
-// console.log(`Resultado: ${parseInt(n1) % parseInt(n2)}`) 
-// console.log(`Resultado: ${parseInt(n1) / parseInt(n2)}`) 
-// console.log(`Resultado: ${parseInt(n1) - parseInt(n2)}`) 
-// var n1 = "25"
-// var n2 = "2"
-//                                           //true  //false
-// resultado = !(parseInt(n1)%parseInt(2)) ? "par" : "impar";
-// console.log(`O numero é: ${resultado}`)
+// // console.log("Olá Mundor")
+// // console.log(`Resultado: ${parseInt(n1) + parseInt(n2)}`)
+// // console.log(`Resultado: ${parseInt(n1) % parseInt(n2)}`) 
+// // console.log(`Resultado: ${parseInt(n1) / parseInt(n2)}`) 
+// // console.log(`Resultado: ${parseInt(n1) - parseInt(n2)}`) 
+// // var n1 = "25"
+// // var n2 = "2"
+// //                                           //true  //false
+// // resultado = !(parseInt(n1)%parseInt(2)) ? "par" : "impar";
+// // console.log(`O numero é: ${resultado}`)
 
-let frutas = ["banana","maça","morango","carambola","acerola"];
-// console.table(frutas);
+// let frutas = ["banana","maça","morango","carambola","acerola"];
+// // console.table(frutas);
 
-// frutas.push("melão");
-// frutas.pop();
+// // frutas.push("melão");
+// // frutas.pop();
 
-let frutaQEuQuero = frutas.indexOf("acerola")
+// let frutaQEuQuero = frutas.indexOf("acerola")
 
-frutas.splice(frutaQEuQuero, 1)
-console.table(frutas)
+// frutas.splice(frutaQEuQuero, 1)
+// console.table(frutas)
 
-frutas.forEach( (fruta) => {
-    console.log(`Olha ele: ${fruta}`)
-} )
+// frutas.forEach( (fruta) => {
+//     console.log(`Olha ele: ${fruta}`)
+// }
+
+let inputUser = document.getElementById('idNm');
+console.log(inputUser);
