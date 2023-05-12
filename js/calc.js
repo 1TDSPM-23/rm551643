f49490ac26ecf9feb7ff93d13bde1055e2dd9d94
jonathanmtsz <rm551643@fiap.com.br>
2023-05-12 10:50:55 -0300

    JS 12 05

---
+++ js/calc.js
@@ -0,0 +1,32 @@
+
+operadores = ["+","-","/","*","%"]
+function NumeroNoVisor(botao,visor){
+    operadores = 
+    function OperadorRepetido(operador){
+        if(visor.value[visor.value.length-1] == operador || visor.value[visor.value.length-1] == "-"){
+            visor.value.slice(1);
+        } else {
+            visor.value += botao.value;
+        }
+
+    } else {
+        visor.value += botao.value;
+    }
+    }
+    
+function OperadorNoVisor(botao,visor){
+
+}
+
+const resultadoDasOperaçoes = (visor) =>{
+    //eval(expressao), eval pega um string em formato de expressão e transforma em string
+    try {
+        visor.value = eval(visor.value);
+    } catch (error) {
+            if(visor.value[visor.value.length] == "+" || visor.value[visor.value.length] == "-"){
+                visor.value = visor.value.slice(visor.value.length-1,1);
+            } else{
+
+            }
+        }
+    }
